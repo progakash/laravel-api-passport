e051ae404e4fc64f096073b76377e8d989460a79
akash-ju16 <akash.ju.msc@gmail.com>
2022-08-21 18:14:29 +0600

    passport api

---
+++ app/Http/Controllers/API/RegistrationController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Hash;
+
+class RegistrationController extends Controller
+{
+    public function registerUser(Request $request){
+
+        // $data = $request->validate([
+        //     'name' => 'required',
+        //     'email' => 'required|email',
+        //     'password' => 'required',
+        // ]);
+
+        $validator = Validator::make($request->all(), [
+                  'name' => 'required',
+                'email' => 'required|email',
+                'password' => 'required',
+        ]);
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+
+        $data['password'] = bcrypt($request->password);
+        $data = $request->all();
+
+        $user = User::create($data);
+
+        $token = $user->createToken('API_Token')->accessToken;
+
+        return response([ 'user' => $user, 'token' => $token]);
+        
+    }
+
+    public function loginUser(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'email'=>'required',
+            'password'=>'required',
+        ]);
+
+
+        if($validator->fails()){
+            return response([ 'error' => 'unauthorized']);
+        }
+
+        $user = User::where('email', $request->email)->first();
+            if ($user) {
+                if (Hash::check($request->password, $user->password)) {
+                    $token = $user->createToken('Grant_Client')->accessToken;
+                    $response = ['token' => $token];
+                    return response($response, 200);
+                } else {
+                    $response = ["message" => "Password mismatch"];
+                    return response($response, 422);
+                }
+            } else {
+                $response = ["message" =>'User does not exist'];
+                return response($response, 422);
+            }
+
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PeopleController;
+use App\Http\Controllers\API\RegistrationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,18 +16,21 @@ use App\Http\Controllers\PeopleController;
 |
 */
 
+Route::post('register', [RegistrationController::class, 'registerUser']);
+Route::post('login', [RegistrationController::class, 'loginUser']);
+
 Route::get('/test', function(Request $request){
     return 'authenticated';
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/apilist', [PeopleController::class, 'allList']);
 
-Route::post('login', [PeopleController::class, 'login']);
-Route::post('register', [PeopleController::class, 'register']);
+// Route::post('login', [PeopleController::class, 'login']);
+// Route::post('register', [PeopleController::class, 'register']);
 // Route::group(['middleware' => 'auth:api'], function(){
 // Route::post('details', 'API\UserController@details');
 // });
